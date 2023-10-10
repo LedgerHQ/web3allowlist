615a83ab3d62e71f414cf45e004419726a5a20d5
William Veal Phan <william.veal-phan@ledger.fr>
2023-10-10 16:43:54 +0200

    Update validate-and-merge-cli.scala

--- .github/scripts/validate-and-merge-cli.scala
+++ .github/scripts/validate-and-merge-cli.scala
@@ -1,10 +1,10 @@
 // Description: Validate and merge the CLI
-//> using scala "3.3.0"
+//> using scala "3.3.1"
 //> using repository "https://NOBODY:${{ GITHUB_TOKEN }}@maven.pkg.github.com/LedgerHQ/_"
-//> using lib "io.circe::circe-generic:0.14.5"
-//> using lib "io.circe::circe-parser:0.14.5"
+//> using lib "io.circe::circe-generic:0.14.6"
+//> using lib "io.circe::circe-parser:0.14.6"
 //> using lib "com.lihaoyi::os-lib:0.9.1"
-//> using lib "co.ledger::backend-commons-circe:2.0.0"
+//> using lib "co.ledger::backend-commons-circe:3.1.1"
 
 import os.*
 import language.deprecated.symbolLiterals

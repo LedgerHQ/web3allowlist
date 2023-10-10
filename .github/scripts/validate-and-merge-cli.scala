ac01cd009f5e6982c95ceb8034fc713be7e7ff66
William Veal Phan <william.veal-phan@ledger.fr>
2023-10-10 16:57:23 +0200

    fix: version backend commons

--- .github/scripts/validate-and-merge-cli.scala
+++ .github/scripts/validate-and-merge-cli.scala
@@ -4,7 +4,7 @@
 //> using lib "io.circe::circe-generic:0.14.3"
 //> using lib "io.circe::circe-parser:0.14.3"
 //> using lib "com.lihaoyi::os-lib:0.9.0"
-//> using lib "co.ledger::backend-commons-circe:1.7.1"
+//> using lib "co.ledger::backend-commons-circe:3.1.1"
 
 import os.*
 import language.deprecated.symbolLiterals
